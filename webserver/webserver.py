13346746458e0dbe5526a60a4ddf55c1c0a92285
Bernard Puc <bernpuc@gmail.com>
2023-07-07 15:02:03 -0400

    Using PORT variable and sending a response string

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -3,23 +3,16 @@ import http.server
 
 PORT = 8000
 
-#Handler = http.server.SimpleHTTPRequestHandler
-
-#with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#    print("serving at port", PORT)
-#    httpd.serve_forever()
-
-
 def some_function():
-    print("some_function got called")
+    return "some_function got called"
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/captureImage':
+        if self.path == '/conditions':
             # Insert your code here
-            some_function()
+            conditions_report = some_function()
 
-        self.send_response(200)
+        self.send_response(200, conditions_report)
 
-httpd = socketserver.TCPServer(("", 8080), MyHandler)
+httpd = socketserver.TCPServer(("", PORT), MyHandler)
 httpd.serve_forever()
