940f51969510326e4ed4f770b3243df30469d52a
Bernard Puc <bernpuc@gmail.com>
2023-07-07 15:15:52 -0400

    Updates to return string

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -4,15 +4,17 @@ import http.server
 PORT = 8000
 
 def some_function():
-    return "some_function got called"
+    return "<http><body>some_function got called<body></http>"
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        conditions_report = "FOOBAR"
         if self.path == '/conditions':
             # Insert your code here
             conditions_report = some_function()
 
         self.send_response(200, conditions_report)
+        self.end_headers()
 
 httpd = socketserver.TCPServer(("", PORT), MyHandler)
 httpd.serve_forever()
