21958047447c4c06af09cd02c6d382bd4b9e7f35
Bernard Puc <bernpuc@gmail.com>
2023-10-16 11:31:43 -0400

    Implements an http server and simple GET interface to parse the temperature data sent from NodeMCU Arduino.

--- pi/relay_prometheus.py
+++ pi/relay_prometheus.py
@@ -1,4 +1,8 @@
 from time import sleep
+# libraries for webserver
+import http.server
+import socketserver
+import re
 
 # libraries for prometheus
 from prometheus_client import Gauge, start_http_server
@@ -12,18 +16,50 @@ def loop():
 
     while True:
         sleep(1)
-        # TODO: Set the temperature from REST API PUT messages
-        temperature = 22.2
-        temp_fahrenheit = temperature * 1.8 + 32
 
-        # Set the gauges
-        gt.labels('celsius').set(temperature)
-        gt.labels('fahrenheit').set(temp_fahrenheit)
+
+class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if re.search('/update*', self.path):
+            self.send_response(200)
+
+            #Parse the temperature value from the path
+            regex = r"=\d+\.\d+"
+            match = re.search(regex, self.path)
+            if match:
+                value = match.group(0)[1:]
+                gt.labels('fahrenheit').set(value)
+        else:
+            self.send_response(403)
+        self.end_headers()
+        sleep(1)
+
+# Define a function to start the web server
+def start_web_server():
+    PORT = 8010
+    DIRECTORY = "."
+
+    Handler = MyRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+
+    print(f"Serving at http://localhost:{PORT}")
+
+    # TODO: Set the temperature from REST API PUT messages
+    temperature = 22.2
+    temp_fahrenheit = temperature * 1.8 + 32
+
+    # Set the gauges
+    gt.labels('celsius').set(temperature)
+    gt.labels('fahrenheit').set(temp_fahrenheit)
+    httpd.serve_forever()
 
 
 if __name__ == "__main__":
     # prometheus will scrape at this address for metrics
     metrics_port = 8002
     start_http_server(metrics_port)
-    loop()
+
+    # Create a new process for the web server
+    start_web_server()
+#    loop()
 
