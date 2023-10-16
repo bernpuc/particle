05af7ce586e7c4e326eaffde34531c0596163c02
Bernard Puc <bernpuc@gmail.com>
2023-10-16 15:37:22 -0400

    Cleaned up code

--- pi/relay_prometheus.py
+++ pi/relay_prometheus.py
@@ -1,59 +1,54 @@
-from time import sleep
+'''
+This script starts two servers. First the prometheus_client server is started on a background thread.
+This is the metrics port which provides the prometheus scraper with metrics data.
+The second server is a simple REST API to receive data from an external sensor via an HTTP GET.
+URI string format is /update?temperature=XX.XX. We use a regex to extract the temperature data from the string.
+'''
+import re
+
 # libraries for webserver
 import http.server
 import socketserver
-import re
 
 # libraries for prometheus
 from prometheus_client import Gauge, start_http_server
 
-# Prometheus Gauges for temperature, humidity, and air quality
+# Prometheus Gauge for temperature
 gt = Gauge('temperature', 'Temperature from sht40 sensor', ['scale'])
-gt.labels('celsius')
 gt.labels('fahrenheit')
 
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if re.search('/update*', self.path):
+        #Parse the temperature value from the path
+        regex = r"=\d+\.\d+"
+        match = re.search(regex, self.path)
+        if match:
             self.send_response(200)
-
-            #Parse the temperature value from the path
-            regex = r"=\d+\.\d+"
-            match = re.search(regex, self.path)
-            if match:
-                value = match.group(0)[1:]
-                gt.labels('fahrenheit').set(value)
+            value = match.group(0)[1:]
+            gt.labels('fahrenheit').set(value)
         else:
-            self.send_response(403)
+            self.send_response(404)
         self.end_headers()
-        sleep(1)
+
 
 # Define a function to start the web server
-def start_web_server():
-    PORT = 8010
-    DIRECTORY = "."
+def start_web_server(PORT):
 
     Handler = MyRequestHandler
     httpd = socketserver.TCPServer(("", PORT), Handler)
 
     print(f"Serving at http://localhost:{PORT}")
-
-    # TODO: Set the temperature from REST API PUT messages
-    temperature = 22.2
-    temp_fahrenheit = temperature * 1.8 + 32
-
-    # Set the gauges
-    gt.labels('celsius').set(temperature)
-    gt.labels('fahrenheit').set(temp_fahrenheit)
     httpd.serve_forever()
 
 
 if __name__ == "__main__":
+    # started on a separate thread
     # prometheus will scrape at this address for metrics
     metrics_port = 8002
     start_http_server(metrics_port)
 
     # Create a new process for the web server
-    start_web_server()
+    rest_port = 8010
+    start_web_server(rest_port)
 
