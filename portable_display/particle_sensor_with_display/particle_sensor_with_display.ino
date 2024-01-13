8361166f895933742f6c379dc1394d0a04c0228f
Bernard Puc <bernpuc@gmail.com>
2024-01-13 12:35:51 -0500

    Adding AQI calculation

--- portable_display/particle_sensor_with_display/particle_sensor_with_display.ino
+++ portable_display/particle_sensor_with_display/particle_sensor_with_display.ino
@@ -31,11 +31,12 @@ void UpdateAQI(uint16_t count);
 void UpdatePM1(uint16_t count);
 void UpdatePM2(uint16_t count);
 void UpdatePM10(uint16_t count);
+uint16_t AQICalculator(uint16_t pm25, uint16_t pm10);
 
 void setup(void){
   Serial1.begin(9600);        // Communication with PMS5003 sensor
 
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);       // Start the Serial communication to send messages to the computer
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -56,11 +57,10 @@ void setup(void){
   
 }
 
-
 void loop(void){
   if (pms.read(data))
   {
-    UpdateAQI(0);
+    UpdateAQI(AQICalculator(data.PM_AE_UG_2_5, data.PM_AE_UG_10_0));
     UpdatePM1(data.PM_AE_UG_1_0);
     UpdatePM2(data.PM_AE_UG_2_5);
     UpdatePM10(data.PM_AE_UG_10_0);
@@ -72,67 +72,67 @@ void loop(void){
 void UpdateDataHeaders()
 {
   writeHeader1("AQI:");
-  writeHeader2("PM1.0:");
-  writeHeader3("PM2.5:");
-  writeHeader4("PM10:");
+  writeHeader2("PM1.0");
+  writeHeader3("PM2.5");
+  writeHeader4("PM10");
 }
 
 void UpdateAQI(uint16_t count)
 {
-  char buf[6];
-  snprintf(buf, 6, "%5d", count);
+  char buf[5];
+  snprintf(buf, 5, "%4d", count);
   writeData1(buf);
 }
 void UpdatePM1(uint16_t count)
 {
-  char buf[6];
-  snprintf(buf, 6, "%5d", count);
+  char buf[5];
+  snprintf(buf, 5, "%4d", count);
   writeData2(buf);
 }
 
 void UpdatePM2(uint16_t count)
 {
-  char buf[6];
-  snprintf(buf, 6, "%5d", count);
+  char buf[5];
+  snprintf(buf, 5, "%4d", count);
   writeData3(buf);
 }
 
 void UpdatePM10(uint16_t count)
 {
-  char buf[6];
-  snprintf(buf, 6, "%5d", count);
+  char buf[5];
+  snprintf(buf, 5, "%4d", count);
   writeData4(buf);
 }
 
 void writeData1(char *buf)
 {
-  display.fillRect(49, 0, 128, 16, SSD1306_BLACK);
+  display.fillRect(64, 0, 128, 16, SSD1306_BLACK);
   display.setTextSize(2); // Draw 2X-scale text
-  display.setCursor(49, 0);
+  display.setCursor(64, 0);
   display.println(buf);
   display.display();
 }
 void writeData2(char *buf)
 {
-  display.fillRect(49, 16, 128, 16, SSD1306_BLACK);
+  display.fillRect(64, 16, 128, 16, SSD1306_BLACK);
   display.setTextSize(2); // Draw 2X-scale text
-  display.setCursor(49, 16);
+  display.setCursor(64, 16);
   display.println(buf);
   display.display();
 }
 void writeData3(char *buf)
 {
-  display.fillRect(49, 32, 128, 16, SSD1306_BLACK);
+  display.fillRect(64, 32, 128, 16, SSD1306_BLACK);
   display.setTextSize(2); // Draw 2X-scale text
-  display.setCursor(49, 32);
+  display.setCursor(64, 32);
   display.println(buf);
   display.display();
 }
 void writeData4(char *buf)
 {
-  display.fillRect(49, 48, 128, 16, SSD1306_BLACK);
+  display.fillRect(64, 48, 128, 16, SSD1306_BLACK);
   display.setTextSize(2); // Draw 2X-scale text
-  display.setCursor(49, 48);
+  display.setCursor(64, 48);
   display.println(buf);
   display.display();
 }
@@ -148,24 +148,87 @@ void writeHeader1(char *buf)
 void writeHeader2(char *buf)
 {
   display.fillRect(0, 16, 128, 16, SSD1306_BLACK);
-  display.setTextSize(1);
-  display.setCursor(0, 20);
+  display.setTextSize(2);
+  display.setCursor(0, 16);
   display.println(buf);
   display.display();
 }
 void writeHeader3(char *buf)
 {
   display.fillRect(0, 32, 128, 16, SSD1306_BLACK);
-  display.setTextSize(1);
-  display.setCursor(0, 36);
+  display.setTextSize(2);
+  display.setCursor(0, 32);
   display.println(buf);
   display.display();
 }
 void writeHeader4(char *buf)
 {
   display.fillRect(0, 48, 128, 16, SSD1306_BLACK);
-  display.setTextSize(1);
-  display.setCursor(0, 52);
+  display.setTextSize(2);
+  display.setCursor(0, 48);
   display.println(buf);
   display.display();
+}
+
+uint16_t AQICalculator(uint16_t pm25, uint16_t pm10)
+{
+  const uint8_t aqi[][2] = {
+    {0, 50},
+    {51, 100},
+    {101, 150},
+    {151, 200},
+    {201, 300},
+    {301, 400},
+    {401, 500},
+  }; 
+
+  const uint16_t pollutant_pm10_0[][2]{
+    {0, 54},
+    {55, 154},
+    {155, 254},
+    {255, 354},
+    {355, 424},
+    {425, 504},
+    {505, 604},
+  };
+
+  const uint16_t pollutant_pm2_5[][2]{
+    {0.0, 12},
+    {13, 35},
+    {36, 55},
+    {56, 150},
+    {151, 250},
+    {251, 350},
+    {351, 500},
+  };
+
+  uint16_t bplo = 0;
+  uint16_t bphi = 0;
+  uint16_t aqilo = 0;
+  uint16_t aqihi = 0;
+  uint16_t aqi_2_5 = 0;
+  uint16_t aqi_10 = 0;
+
+  for (int idx = 0; idx < 7; idx++)
+  {
+    bplo = pollutant_pm2_5[idx][0];
+    bphi = pollutant_pm2_5[idx][1];
+    if (pm25 >= bplo && pm25 <= bphi)
+    {
+      aqilo = aqi[idx][0];
+      aqihi = aqi[idx][1];
+      aqi_2_5 = (float)(aqihi - aqilo) / (float)(bphi - bplo) * (pm25 - bplo) + aqilo;
+    }
+
+    bplo = pollutant_pm10_0[idx][0];
+    bphi = pollutant_pm10_0[idx][1];
+    if (pm10 >= bplo && pm10 <= bphi)
+    {
+      aqilo = aqi[idx][0];
+      aqihi = aqi[idx][1];
+      aqi_10 = (float)(aqihi - aqilo) / (float)(bphi - bplo) * (pm10 - bplo) + aqilo;
+    }
+    
+  }
+  return (aqi_10 > aqi_2_5) ? (uint16_t)aqi_10 : (uint16_t)aqi_2_5;
 }
